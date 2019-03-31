f93f7db3f2a6c7f8e2bcf107d1f9cd1dbda05998
Nidhal <nidhalmesselmani@yahoo.fr>
2019-03-30 18:22:58 -0700

    Initial commit

---
+++ Source/Bedlam/BedlamGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BedlamGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEDLAM_API ABedlamGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
